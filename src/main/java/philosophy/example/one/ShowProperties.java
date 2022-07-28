9405d13af711187bb4abbdcd5ca8c6416365cdce
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2022-07-28 23:44:56 +0300

    Add files via upload

---
+++ src/main/java/philosophy/example/one/ShowProperties.java
@@ -0,0 +1,14 @@
+package philosophy.example.one;
+
+class DataOnly {
+    int i;
+    double d;
+    boolean b;
+
+    DataOnly data = new DataOnly();
+
+    // data.i = 47;  //in java 5
+}
+
+public class ShowProperties {
+}
