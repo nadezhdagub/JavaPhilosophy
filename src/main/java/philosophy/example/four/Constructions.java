8b44112853db0e00315ddf5328f155d954d2631a
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2022-08-12 18:29:30 +0300

    Add files via upload

--- src/main/java/philosophy/example/four/Constructions.java
+++ src/main/java/philosophy/example/four/Constructions.java
@@ -51,5 +51,19 @@ public class Constructions {
             System.out.println(c + " ");
         }
 
+        for (int i = 0; i < 100; i++) {
+            if(i == 74) break;
+            if(i % 9 != 0) continue;
+            System.out.println(i + " ");
+        }
+        System.out.println();
+        int i = 0;
+        while (true) {
+            i++;
+            int j = i * 27;
+            if(j == 1269) break;
+            if(i % 10 != 0) continue;
+            System.out.println(i + "");
+        }
     }
 }
--- src/main/java/philosophy/example/four/Exercises.java
+++ src/main/java/philosophy/example/four/Exercises.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class Exercises {
     static Random r = new Random();
+
     public static void main(String[] args) {
         int max = 100;
         /** 1) Write a program that outputs numbers from 1 to 100.
@@ -36,12 +37,12 @@ public class Exercises {
         if (args.length != 0) {
             max = Integer.parseInt(args[0]);
         }
-        for(int i = 1; i < max; i++) {
+        for (int i = 1; i < max; i++) {
             boolean prime = true;
-            for(int j = 2; j < i; j++)
-                if(i % j == 0)
+            for (int j = 2; j < i; j++)
+                if (i % j == 0)
                     prime = false;
-            if(prime)
+            if (prime)
                 System.out.print(i + " ");
         }
 
@@ -52,24 +53,151 @@ public class Exercises {
          */
         int i1 = 0xaaaaaaaa;
         int i2 = 0x55555555;
-        System.out.println("i1 = "); toBinaryString(i1);
-        System.out.println("i2 = "); toBinaryString(i2);
-        System.out.println("~i1 = "); toBinaryString(~i1);
-        System.out.println("~i2 = "); toBinaryString(~i2);
-        System.out.println("i1 & i1 = "); toBinaryString(i1 & i1);
-        System.out.println("i1 | i1 = "); toBinaryString(i1 | i1);
-        System.out.println("i1 ^ i1 = "); toBinaryString(i1 ^ i1);
-        System.out.println("i1 & i2 = "); toBinaryString(i1 & i2);
-        System.out.println("i1 | i2 = "); toBinaryString(i1 | i2);
-        System.out.println("i1 ^ i2 = "); toBinaryString(i1 ^ i2);
+        System.out.println("i1 = ");
+        toBinaryString(i1);
+        System.out.println("i2 = ");
+        toBinaryString(i2);
+        System.out.println("~i1 = ");
+        toBinaryString(~i1);
+        System.out.println("~i2 = ");
+        toBinaryString(~i2);
+        System.out.println("i1 & i1 = ");
+        toBinaryString(i1 & i1);
+        System.out.println("i1 | i1 = ");
+        toBinaryString(i1 | i1);
+        System.out.println("i1 ^ i1 = ");
+        toBinaryString(i1 ^ i1);
+        System.out.println("i1 & i2 = ");
+        toBinaryString(i1 & i2);
+        System.out.println("i1 | i2 = ");
+        toBinaryString(i1 | i2);
+        System.out.println("i1 ^ i2 = ");
+        toBinaryString(i1 ^ i2);
 
+
+        /** 6) Change the test() method so that it gets two additional arguments
+         * begin and end, and the value of testval was checked for belonging to the range
+         * [ begin, end] (with borders included).
+         */
+        test(10, 6, 20);
+        test(54, 365, 5423);
+
+
+        /** 7) Change exercise 1 so that the exit from the program
+         * is carried out by the break keyword at a value of 99.
+         * Try using the return keyword.
+         */
+        for (int i = 1; i <= 100; i++) {
+            if (i == 99)
+                break;
+            System.out.print(i + " ");
+        }
+
+        /** 8) Create a switch command that outputs a message in each case section.
+         * Place it in a for loop that checks all valid case values.
+         * Each case section must end with the break command.
+         * Then remove the break commands and see what happens.
+         */
+        for (int i = 0; i < 5; i++) {
+            switch (i) {
+                case 1:
+                    System.out.println("case 0");
+                    break;
+                case 2:
+                    System.out.println("case 1");
+                    break;
+                case 3:
+                    System.out.println("case 2");
+                    break;
+                case 4:
+                    System.out.println("case 3");
+                    break;
+                case 5:
+                    System.out.println("case 4");
+                    break;
+                default:
+                    System.out.println("default");
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            switch (i) {
+                case 1:
+                    System.out.println("case 0");
+                case 2:
+                    System.out.println("case 1");
+                case 3:
+                    System.out.println("case 2");
+                case 4:
+                    System.out.println("case 3");
+                case 5:
+                    System.out.println("case 4");
+                default:
+                    System.out.println("default");
+            }
+        }
+
+        /** 9) Fibonacci numbers are a numerical sequence 1,1,2,3,5,8,13,21, 34, etc.,
+         * in which each number, starting from the third, is the sum of the previous two.
+         * Write a method that gets an integer argument and outputs the specified number
+         * of Fibonacci numbers. For example, when running the java command Fibonacci 5
+         * (where Fibonacci is the class name) the sequence should be output 1, 1, 2, 3, 5.
+         */
+        int n = Integer.parseInt(args[0]);
+        for (int i = 1; i < n; i++) {
+            System.out.println(fib(i) + ", ");
+        }
+
+
+        /** 10) Vampires are numbers consisting of an even number
+         * of digits and obtained by multiplying a pair of numbers,
+         * each of which contains half the digits of the result.
+         * The digits are taken from the original number in any order,
+         * terminating zeros are not allowed. Examples:
+         * 1) 1261 =21 *60;
+         * 2) 1827 = 21 *87;
+         * 3) 2187 = 27*81.
+         * Write a program that finds all "vampires" consisting of 4 digits.
+         * ((The problem is proposed by Dan Forhan.)
+         */
+        int[] startDigit = new int[4];
+        int[] productDigit = new int[4];
+        for (int num1 = 10; num1 <= 99; num1++)
+            for (int num2 = num1; num2 <= 99; num2++) {
+                if ((num1 * num2) % 9 != (num1 + num2) % 9) {
+                    continue;
+                }
+                int product = num1 * num2;
+                startDigit[0] = num1 / 10;
+                startDigit[1] = num1 % 10;
+                startDigit[2] = num2 / 10;
+                startDigit[3] = num2 % 10;
+                productDigit[0] = product / 1000;
+                productDigit[1] = (product % 1000) / 100;
+                productDigit[2] = product % 1000 % 100 / 10;
+                productDigit[3] = product % 1000 % 100 % 10;
+                int count = 0;
+                for (int x = 0; x < 4; x++) {
+                    for (int y = 0; y < 4; y++) {
+                        if (productDigit[x] == startDigit[y]) {
+                            count++;
+                            productDigit[x] = -1;
+                            startDigit[y] = -2;
+                            if (count == 4) {
+                                System.out.println(num1 + " * " + num2
+                                        + " : " + product);
+                            }
+                        }
+                    }
+                }
+            }
     }
 
     static void condition() {
         int a = r.nextInt();
         int b = r.nextInt();
         System.out.println("a: " + a + " , " + "b: " + b);
-        if(a > b) {
+        if (a > b) {
             System.out.println("a > b");
         } else if (a < b) {
             System.out.println("a < b");
@@ -86,8 +214,23 @@ public class Exercises {
                     ((i & 0x01) != 0) ? '1' : '0';
             i >>>= 1;
         } while (i != 0);
-        for(int j = bufferPosition; j < 32; j++)
+        for (int j = bufferPosition; j < 32; j++)
             System.out.println(buffer[j]);
         System.out.println();
+    }
+
+    static int test(int testval, int begin, int end) {
+        if ((testval >= begin) && (testval <= end)) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    static int fib(int n) {
+        if ((n <= 2) && (n > 0)) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
     }
 }
