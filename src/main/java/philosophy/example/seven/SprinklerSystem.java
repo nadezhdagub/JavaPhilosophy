265ff91fc804f7298891183f098d3c24c098eae1
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2022-09-22 15:06:20 +0300

    Add files via upload

--- src/main/java/philosophy/example/seven/SprinklerSystem.java
+++ src/main/java/philosophy/example/seven/SprinklerSystem.java
@@ -54,3 +54,49 @@ public class SprinklerSystem {
         System.out.println(sprinklerSystem);
     }
 }
+
+class Cleanser {
+    private String s = "Cleanser";
+    public void append(String a) {
+        s += a;
+    }
+    public void dilute() {
+        append(" dilute()");
+    }
+    public void apply() {
+        append(" apply()");
+    }
+    public void scrub() {
+        append(" scrub()");
+    }
+    public String toString() {
+        return s;
+    }
+    public static void main(String[] args) {
+        Cleanser x = new Cleanser();
+        x.dilute();
+        x.apply();
+        x.scrub();
+        System.out.println(x);
+    }
+}
+
+class Detergent extends Cleanser {
+    public void scrub() {
+        append(" Detergent.scrub()");
+        super.scrub();
+    }
+    public void foam() {
+        append(" foam()");
+    }
+    public static void main(String[] args) {
+        Detergent x = new Detergent();
+        x.dilute();
+        x.apply();
+        x.scrub();
+        x.foam();
+        System.out.println(x);
+        System.out.println("Check base class");
+        Cleanser.main(args);
+    }
+}
