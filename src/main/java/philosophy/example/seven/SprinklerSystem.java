bc600745f5401d1e875dfbb0277587e7a32b49a3
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2022-09-27 23:17:43 +0300

    Add files via upload

--- src/main/java/philosophy/example/seven/SprinklerSystem.java
+++ src/main/java/philosophy/example/seven/SprinklerSystem.java
@@ -57,21 +57,27 @@ public class SprinklerSystem {
 
 class Cleanser {
     private String s = "Cleanser";
+
     public void append(String a) {
         s += a;
     }
+
     public void dilute() {
         append(" dilute()");
     }
+
     public void apply() {
         append(" apply()");
     }
+
     public void scrub() {
         append(" scrub()");
     }
+
     public String toString() {
         return s;
     }
+
     public static void main(String[] args) {
         Cleanser x = new Cleanser();
         x.dilute();
@@ -81,14 +87,62 @@ class Cleanser {
     }
 }
 
+class Art {
+    Art() {
+        System.out.println("KoHCTpyKTop Art");
+    }
+}
+
+class Drawing extends Art {
+    Drawing() {
+        System.out.println("KoHCTpyKTop Drawing");
+    }
+}
+
+class Cartoon extends Drawing {
+    public Cartoon() {
+        System.out.println("KoHCTpyKTop Cartoon");
+    }
+
+    public static void main(String[] args) {
+        Cartoon x = new Cartoon();
+    }
+}
+
+class Game {
+    Game(int i) {
+        System.out.println("KoHCTpyKTop Game");
+    }
+}
+
+class BoardGame extends Game {
+    BoardGame(int i) {
+        super(i);
+        System.out.println("KoHCTpyKTop BoardGame");
+    }
+}
+
+class Chess extends BoardGame {
+    Chess() {
+        super(11);
+        System.out.println("KoHCTpyKTop Chess");
+    }
+
+    public static void main(String[] args) {
+        Chess x = new Chess();
+    }
+}
+
 class Detergent extends Cleanser {
     public void scrub() {
         append(" Detergent.scrub()");
         super.scrub();
     }
+
     public void foam() {
         append(" foam()");
     }
+
     public static void main(String[] args) {
         Detergent x = new Detergent();
         x.dilute();
