3ac90a766cf484c5250b1117f68a8714aa00e9a8
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2022-09-30 23:14:45 +0300

    Add files via upload

--- src/main/java/philosophy/example/seven/SprinklerSystem.java
+++ src/main/java/philosophy/example/seven/SprinklerSystem.java
@@ -133,6 +133,155 @@ class Chess extends BoardGame {
     }
 }
 
+class SpaceShipControls {
+    void up(int velocity) {
+    }
+
+    void down(int velocity) {
+    }
+
+    void left(int velocity) {
+    }
+
+    void right(int velocity) {
+    }
+
+    void forward(int velocity) {
+    }
+
+    void back(int velocity) {
+    }
+
+    void turboBoost() {
+    }
+}
+
+class SpaceShip extends SpaceShipControls {
+    private String name;
+
+    public SpaceShip(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+        SpaceShip protector = new SpaceShip("NSEA Protector");
+    }
+}
+
+class SpaceShipDelegator {
+    private String name;
+    private SpaceShipControls controls = new SpaceShipControls();
+
+    public SpaceShipDelegator(String name) {
+        this.name = name;
+    }
+
+    public void back(int velocity) {
+        controls.back(velocity);
+    }
+
+    public void down(int velocity) {
+        controls.down(velocity);
+    }
+
+    public void forward(int velocity) {
+        controls.forward(velocity);
+    }
+
+    public void left(int velocity) {
+        controls.left(velocity);
+    }
+
+    public void right(int velocity) {
+        controls.right(velocity);
+    }
+
+    public void turboBoost() {
+        controls.turboBoost();
+    }
+
+    public void up(int velocity) {
+        controls.up(velocity);
+    }
+
+    public static void main(String[] args) {
+        SpaceShipDelegator protector = new SpaceShipDelegator("NSEA Protector");
+        protector.forward(100);
+    }
+}
+
+class Plate {
+    Plate(int i) {
+        System.out.println("Constructor Plate");
+    }
+}
+
+class DinnerPlate extends Plate {
+    DinnerPlate(int i) {
+        super(i);
+        System.out.println("Constructor DinnerPlate");
+    }
+}
+
+class Utensil {
+    Utensil(int i) {
+        System.out.println("Constructor Utensil");
+    }
+}
+
+class Spoon extends Utensil {
+    Spoon(int i) {
+        super(i);
+        System.out.println("Constructor Spoon");
+    }
+}
+
+
+class Fork extends Utensil {
+    Fork(int i) {
+        super(i);
+        System.out.println("Constructor Fork");
+    }
+}
+
+class Knife extends Utensil {
+    Knife(int i) {
+        super(i);
+        System.out.println("Constructor Knife");
+    }
+}
+
+class Custom {
+    Custom(int i) {
+        System.out.println("Constructor Custom");
+    }
+}
+
+class PlaceSetting extends Custom {
+    private Spoon sp;
+    private Fork frk;
+    private Knife kn;
+    private DinnerPlate pl;
+
+    public PlaceSetting(int i) {
+        super(i + 1);
+        sp = new Spoon(i + 2);
+        frk = new Fork(i + 3);
+        kn = new Knife(i + 4);
+        pl = new DinnerPlate(i + 5);
+        System.out.println("Constructor PlaceSetting");
+    }
+
+    public static void main(String[] args) {
+        PlaceSetting x = new PlaceSetting(9);
+    }
+}
+
+
 class Detergent extends Cleanser {
     public void scrub() {
         append(" Detergent.scrub()");
