3f01e2072476c6be4b355056986b21f7c70b76bc
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2022-10-08 18:55:01 +0300

    Add files via upload

--- src/main/java/philosophy/example/seven/SprinklerSystem.java
+++ src/main/java/philosophy/example/seven/SprinklerSystem.java
@@ -281,7 +281,6 @@ class PlaceSetting extends Custom {
     }
 }
 
-
 class Detergent extends Cleanser {
     public void scrub() {
         append(" Detergent.scrub()");
@@ -303,3 +302,91 @@ class Detergent extends Cleanser {
         Cleanser.main(args);
     }
 }
+
+class Shape {
+    Shape(int i) {
+        System.out.println("Constructor Shape");
+    }
+
+    void dispose() {
+        System.out.println("Ending Shape");
+    }
+}
+
+class Circle extends Shape {
+    Circle(int i) {
+        super(i);
+        System.out.println("Draw circle Circle");
+    }
+
+    void dispose() {
+        System.out.println("Clean circle Circle");
+        super.dispose();
+    }
+}
+
+class Triangle extends Shape {
+    Triangle(int i) {
+        super(i);
+        System.out.println("Draw triangle Triangle");
+    }
+
+    void dispose() {
+        System.out.println("Clean triangle Triangle");
+        super.dispose();
+    }
+}
+
+class Line extends Shape {
+    private int start, end;
+
+    Line(int start, int end) {
+        super(start);
+        this.start = start;
+        this.end = end;
+        System.out.println("Draw line Line: " + start + ", " + end);
+    }
+
+    void dispose() {
+        System.out.println("Clean line Line: " + start + ", " + end);
+        super.dispose();
+    }
+}
+
+class CADSystem extends Shape {
+    private Circle c;
+    private Triangle t;
+    private Line[] lines = new Line[3];
+
+    public CADSystem(int i) {
+        super(i + 1);
+        for (int j = 0; j < lines.length; j++) {
+            lines[j] = new Line(j, j * j);
+        }
+        c = new Circle(1);
+        t = new Triangle(1);
+        System.out.println("Combinated constructor");
+    }
+
+    void dispose() {
+        System.out.println("CADSystem.dispose()");
+        // Завершение осуществляется в порядке,
+        // обратном порядку инициализации
+        t.dispose();
+        c.dispose();
+        for (int i = lines.length - 1; i >= 0; i--) {
+            lines[i].dispose();
+        }
+        super.dispose();
+    }
+
+    public static void main(String[] args) {
+        CADSystem x = new CADSystem(47);
+        try {
+        // kod and exception handling
+        } finally {
+            x.dispose();
+        }
+    }
+}
+
