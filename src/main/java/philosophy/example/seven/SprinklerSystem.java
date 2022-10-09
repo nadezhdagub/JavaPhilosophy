834a3a85a1da548640ee833cb1c7d75f4b7e2071
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2022-10-10 00:19:41 +0300

    Add files via upload

--- src/main/java/philosophy/example/seven/SprinklerSystem.java
+++ src/main/java/philosophy/example/seven/SprinklerSystem.java
@@ -390,3 +390,38 @@ class CADSystem extends Shape {
     }
 }
 
+class Homer {
+    char doh(char c) {
+        System.out.println("doh(char)");
+        return 'd';
+    }
+    float doh(float f) {
+        System.out.println("doh(float)");
+        return 1.0f;
+    }
+}
+
+class Milhouse {}
+
+class Bart extends Homer {
+    void doh(Milhouse m) {
+        System.out.println("doh(Milhouse)");
+    }
+}
+
+class Hide {
+    public static void main(String[] args) {
+        Bart b = new Bart();
+        b.doh(1);
+        b.doh('x');
+        b.doh(1.0f);
+        b.doh(new Milhouse());
+    }
+}
+
+//class Lisa extends Homer {
+//    @Override
+//    void doh(Milhouse m) {
+//        System.out.println("doh(Milhouse)");
+//    }
+//}
