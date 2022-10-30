a4eb10ca820476fc815f9cdadce170cde9285c27
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2022-10-31 00:54:12 +0300

    Add files via upload

--- src/main/java/philosophy/example/seven/SprinklerSystem.java
+++ src/main/java/philosophy/example/seven/SprinklerSystem.java
@@ -523,7 +523,37 @@ class FinalData {
     }
 }
 
+class Poppet {
+    private int i;
 
+    Poppet(int ii) {
+        i = ii;
+    }
+}
+
+class BlankFinal {
+    private final int i = 0; // Инициализированная константа
+    private final int j; // Пустая константа
+    private final Poppet p; // Пустая константа -ссылка
+
+    // Пустые константы НЕОБХОДИМО инициализировать
+// в конструкторе :
+    public BlankFinal() {
+        j = 1; // Инициализация пустой константы
+        p = new Poppet(1); // Инициализация пустой неизменной ссылки
+    }
+
+    public BlankFinal(int x) {
+        j = x; // Инициализация пустой константы
+        p = new Poppet(x); // Инициализация пустой неизменной ссылки
+    }
+
+    public static void main(String[] args) {
+        new BlankFinal();
+        new BlankFinal(47);
+
+    }
+}
 //class Lisa extends Homer {
 //    @Override
 //    void doh(Milhouse m) {
