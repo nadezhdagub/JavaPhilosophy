c7d01339c1ebd83ae80a6b8b366c6aab72062e4c
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2022-11-04 00:02:34 +0300

    Add files via upload

--- src/main/java/philosophy/example/seven/Exercises.java
+++ src/main/java/philosophy/example/seven/Exercises.java
@@ -1,7 +1,38 @@
 package philosophy.example.seven;
 
-import static java.lang.Math.log;
+class Simple {
+    String s;
+    protected Simple(String ss) {this.s = ss;}
+    public String toString() { return s;}
+    public void setString(String sn) {this.s = sn;}
+}
 
 public class Exercises {
+    /**
+     * 1) Create a simple class. In the second class,
+     * define a reference to an object of the first class.
+     * Use deferred initialization to create instances of this class.
+     */
+    Simple simple;
+    String s;
+    public Exercises(String ss) {
+        this.s = ss;
+    }
+    private Simple lazy() {
+        if(simple == null) {
+            System.out.println("Creating Simple");
+            simple = new Simple(s);
+        }
+        return simple;
+    }
 
+    public static void main(String[] args) {
+        /**
+         * 1)
+         */
+        Exercises exercises = new Exercises("In");
+        exercises.lazy();
+
+        
+    }
 }
