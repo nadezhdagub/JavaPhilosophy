6782579b94b0db51723e890a751cb7f660267709
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2022-11-05 02:24:27 +0300

    Add files via upload

--- src/main/java/philosophy/example/seven/Exercises.java
+++ src/main/java/philosophy/example/seven/Exercises.java
@@ -2,12 +2,90 @@ package philosophy.example.seven;
 
 class Simple {
     String s;
-    protected Simple(String ss) {this.s = ss;}
-    public String toString() { return s;}
-    public void setString(String sn) {this.s = sn;}
+
+    protected Simple(String ss) {
+        this.s = ss;
+    }
+
+    public String toString() {
+        return s;
+    }
+
+    public void setString(String sn) {
+        this.s = sn;
+    }
 }
 
-public class Exercises {
+class Cleanser1 {
+    private String s = "Cleanser";
+
+    public void append(String а) {
+        s += а;
+    }
+
+    public void dilute() {
+        append(" dilute()");
+    }
+
+    public void apply() {
+        append(" applyQ");
+    }
+
+    public void scrub() {
+        append(" scrub()");
+    }
+
+    public String toString() {
+        return s;
+    }
+
+    public static void main(String[] args) {
+        Cleanser x = new Cleanser();
+        x.dilute();
+        x.apply();
+        x.scrub();
+        System.out.println(x);
+    }
+
+}
+
+class Detergent1 extends Cleanser1 {
+    // Изменяем метод:
+    public void scrub() {
+        append(" Detergent.scrub()");
+        super.scrub(); // Вызываем метод базового класса
+    }
+
+    // Добавляем новые методы к интерфейсу:
+    public void foam() {
+        append("foamQ");
+    }
+
+    // Проверяем новый класс:
+    public static void main(String[] args) {
+        Detergent x = new Detergent();
+        x.dilute();
+        x.apply();
+        x.scrub();
+        x.foam();
+        System.out.println(x);
+        System.out.println("проверяем базовый класс");
+        Cleanser.main(args);
+    }
+}
+
+class NewDetergent extends Detergent {
+    public void scrub() {
+        append(" NewDetergent.scrub()");
+        super.scrub(); // Doesn't have to be first
+    }
+
+    public void sterilize() {
+        append(" sterilize()");
+    }
+}
+
+public class Exercises extends Detergent {
     /**
      * 1) Create a simple class. In the second class,
      * define a reference to an object of the first class.
@@ -15,11 +93,16 @@ public class Exercises {
      */
     Simple simple;
     String s;
+
+    public Exercises() {
+    }
+
     public Exercises(String ss) {
         this.s = ss;
     }
+
     private Simple lazy() {
-        if(simple == null) {
+        if (simple == null) {
             System.out.println("Creating Simple");
             simple = new Simple(s);
         }
@@ -33,6 +116,15 @@ public class Exercises {
         Exercises exercises = new Exercises("In");
         exercises.lazy();
 
-        
+        /**
+         * 2) Declare a new class derived from Detergent.
+         * Redefine the scrub() method
+         * and add a new method named sterilize().
+         */
+        Exercises ex = new Exercises();
+        ex.scrub();
+        System.out.println(ex);
+
+
     }
 }
