831a9a7d99d1e297fe5833211273f120df30d5a8
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2022-11-12 00:54:10 +0300

    Add files via upload

--- src/main/java/philosophy/example/seven/Exercises.java
+++ src/main/java/philosophy/example/seven/Exercises.java
@@ -169,7 +169,30 @@ class DerivedTwoConstructors extends BaseNonDefault {
     }
 }
 
-public class Exercises extends Detergent {
+class Component1 {
+    public Component1() { System.out.println("Component1"); }
+}
+class Component2 {
+    public Component2() { System.out.println("Component2"); }
+}
+class Component3 {
+    public Component3() { System.out.println("Component3"); }
+}
+
+class Root {
+    Component1 c1 = new Component1();
+    Component2 c2 = new Component2();
+    Component3 c3 = new Component3();
+    public Root() { System.out.println("Root"); }
+}
+class Stem extends Root {
+    Component1 c1 = new Component1();
+    Component2 c2 = new Component2();
+    Component3 c3 = new Component3();
+    public Stem() { System.out.println("Stem"); }
+}
+
+    public class Exercises extends Detergent {
     /**
      * 1) Create a simple class. In the second class,
      * define a reference to an object of the first class.
@@ -251,5 +274,14 @@ public class Exercises extends Detergent {
          */
         new DerivedTwoConstructors();
         new DerivedTwoConstructors(47);
+
+        /**
+         * 9) Create a Root class containing instances of each of the classes
+         * (also created by you) Component, Component2 and Component3.
+         * Declare the Stem class derived from the Root class, so that it
+         * also contains instances of each of the Component classes mentioned.
+         * Each class must contain a default constructor that outputs a message about its call.
+         */
+        new Stem();
     }
 }
