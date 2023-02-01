e010d038251883379a12cf95f5e28a809c2239f4
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2023-02-02 00:18:59 +0300

    Update Polimorphyzm.java

--- src/main/java/philosophy/example/eight/Polimorphyzm.java
+++ src/main/java/philosophy/example/eight/Polimorphyzm.java
@@ -114,3 +114,9 @@ public class Polimorphyzm {
         tune(flute);       //восход\щее преобразование
     }
 }
+    
+class Instrument {
+void play(Note n) {print("Instrument.play()" + n ); }
+String what() { return "Instrument"; }
+void adjust() { print("Adjust ing Instrument" ) ; }
+}   
