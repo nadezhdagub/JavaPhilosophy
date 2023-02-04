a9b3519311b23f6f83038467dfb619ebf06711bb
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2023-02-04 22:12:17 +0300

    Update Polimorphyzm.java

--- src/main/java/philosophy/example/eight/Polimorphyzm.java
+++ src/main/java/philosophy/example/eight/Polimorphyzm.java
@@ -126,3 +126,9 @@ void play ( Note n) { print ("Wind.play()" + n ) ; }
 String what() { return "Wind"; }
 void adjust() { print ( "Adjusting Wind"); }
                      }
+
+class Percussion extends Instrument {
+void play (Note n) { print ("Percussion.play()" + n ) ; }
+String what() { return "Percussion" ; }
+void adjust() { print ("Adjusting Percussion" ) ; }
+}    
