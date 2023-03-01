d4ae666771a23c57793c02dbd36db06c03a94616
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2023-03-02 02:27:17 +0300

    Update Polimorphyzm.java

--- src/main/java/philosophy/example/eight/Polimorphyzm.java
+++ src/main/java/philosophy/example/eight/Polimorphyzm.java
@@ -190,3 +190,10 @@ class Super {
 public int field = 0 ;
 public int getField() { return field ; }
 }                              
+
+class Sub extends Super {
+public int field = 1 ;
+public int getField() { return field ; }
+public int getSuperField() { return super.field ; }
+}
+                              
