634f9fb5a32895538f98ff9bc7586a0ef3fb687b
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2023-03-09 23:20:35 +0300

    Update Polimorphyzm.java

--- src/main/java/philosophy/example/eight/Polimorphyzm.java
+++ src/main/java/philosophy/example/eight/Polimorphyzm.java
@@ -197,3 +197,16 @@ public int getField() { return field ; }
 public int getSuperField() { return super.field ; }
 }
                               
+class FieldAccess{
+public static void main (String[] args) {
+Super sup = new Sub(); // Upcast
+System.out.println("sup.field= " + sup.field +
+", sup.getField() = " + sup.getField());
+Sub sub = new Sub();
+System.out.println( "sub.field = " +
+sub.field + " , sub.getField() = " +
+sub. getField() +
+" , sub.getSuperField() = " +
+sub.getSuperField()) ;
+}
+}
