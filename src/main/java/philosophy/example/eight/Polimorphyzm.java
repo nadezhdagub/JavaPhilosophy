d94f81dab4d14293f6b2ec9bc2de7024796db1cb
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2023-03-13 23:43:40 +0300

    Update Polimorphyzm.java

--- src/main/java/philosophy/example/eight/Polimorphyzm.java
+++ src/main/java/philosophy/example/eight/Polimorphyzm.java
@@ -220,3 +220,11 @@ return "Базовая версия dynamicGet()" ;
 }
 }
                               
+class StaticSub extends StaticSuper {
+public static String staticGet( ) {
+return "Производная версия staticGet() " ;
+}
+public String dynamicGet() {
+return "Производная версия dynamicGet()" ;
+}
+}
