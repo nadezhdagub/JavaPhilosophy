f3f449dd0e998c507020402d73639f7aaa8899b7
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2023-03-29 03:47:19 +0300

    Update Polimorphyzm.java

--- src/main/java/philosophy/example/eight/Polimorphyzm.java
+++ src/main/java/philosophy/example/eight/Polimorphyzm.java
@@ -248,6 +248,6 @@ class Cheese {
 Cheese() { print( "Cheese()" ); }
                 }
                               
-cla s s L e t t u c e {
+clas s L e t t u c e {
 L e t t u c e ( ) { p r i n t ( " L e t t u c e ( ) " ); }
 }
