d9666bc2eb45bf67924bb791cd08ca9ac1845e27
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2023-04-08 10:34:55 +0300

    Update Polimorphyzm.java

--- src/main/java/philosophy/example/eight/Polimorphyzm.java
+++ src/main/java/philosophy/example/eight/Polimorphyzm.java
@@ -269,3 +269,14 @@ public static void main( String[ ] args ) {
 new Sandwich() ;
 }
     }                              
+
+c l a s s C h a r a c t e r i s t i c {
+p r i v a t e S t r i n g s ;
+C h a r a c t e r i s t i c ( S t r i n g s ) {
+t h i s . s = s ;
+p r in t( " C o 3 fla e M C h a r a c t e r i s t i c " + s ) ;
+}
+p r o t e c t e d v o id d is p o s e ( ) {
+p rin t("3 a e e p u ja e M C h a r a c t e r i s t i c " + s ) ;
+}
+    }
