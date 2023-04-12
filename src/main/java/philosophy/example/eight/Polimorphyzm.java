ab07e289dc2bc5d981bd471b548709a7f03b7486
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2023-04-12 22:15:09 +0300

    Update Polimorphyzm.java

--- src/main/java/philosophy/example/eight/Polimorphyzm.java
+++ src/main/java/philosophy/example/eight/Polimorphyzm.java
@@ -283,7 +283,7 @@ print("3aeepujae M Characteristic" + s ) ;
 
 c l a s s D e s c r i p t io n {
 p r i v a t e S t r i n g s ;
-D e s c r i p t i o n ( String s) {
+Description( String s) {
 this.s = s;
 print( "Co3flae M Description" + s);
 }
