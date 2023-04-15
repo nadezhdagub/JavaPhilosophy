9082626f6dbf3e3976acdc7e0a72ea8c2f476267
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2023-04-15 16:57:27 +0300

    Update Polimorphyzm.java

--- src/main/java/philosophy/example/eight/Polimorphyzm.java
+++ src/main/java/philosophy/example/eight/Polimorphyzm.java
@@ -310,7 +310,7 @@ p . d i s p o s e ( ) ;
                               
 // животное
 c l a s s A n itn a l e x te n d s L iv in g C r e a t u r e {
-p r i v a t e Characteristic p = new Characteristic( "H MeeT сердце");
+private Characteristic p = new Characteristic( "H MeeT сердце");
 private Descriptiont = new Description('^nBOTHoe, не растение");
 Animal() { print("Animal()"); }
 protected void dispose() {
