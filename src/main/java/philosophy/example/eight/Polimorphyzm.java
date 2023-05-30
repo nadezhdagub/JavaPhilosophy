0988fd6752fc648df2d25ab48acc22ec8710fdda
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2023-05-30 22:50:39 +0300

    Update Polimorphyzm.java

--- src/main/java/philosophy/example/eight/Polimorphyzm.java
+++ src/main/java/philosophy/example/eight/Polimorphyzm.java
@@ -428,3 +428,14 @@ Grain process() { return new Grain(); }
 class WheatMill extends Mill {
 Wheat process() { return new Wheat()j }
 }      
+
+class CovariantReturn {
+public static void main(String[] args) {
+Mill m = new Mill();
+Grain g - m.process();
+System.out.println(g);
+m = new WheatMill();
+g = m.process();
+System.out.println(g)j
+    }
+    }      
