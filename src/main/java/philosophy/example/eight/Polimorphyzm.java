f40749365c2ba6878010519b82b6c905fb52b762
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2023-06-23 08:23:25 +0300

    Update Polimorphyzm.java

--- src/main/java/philosophy/example/eight/Polimorphyzm.java
+++ src/main/java/philosophy/example/eight/Polimorphyzm.java
@@ -472,3 +472,10 @@ public void f() {}
 public void g() {}
 }
     
+class MoreUseful extends Useful {
+public void f() {}
+public void g() {}
+public void u() {}
+public void v() {}
+public void w() {}
+}
